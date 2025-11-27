3e836d3f0f1e14b149a142db04e34668f89e2da9
Sradhakp14 <sradhakp14@gmail.com>
2025-11-27 11:35:43 +0530

    Third committ

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -17,28 +17,16 @@ import { protectAdmin } from "../middleware/adminMiddleware.js";
 
 const router = express.Router();
 
-
-// ================= USER ROUTES =================
-
-// Create order
 router.post("/", protect, createOrder);
 
-// User order history
 router.get("/myorders", protect, getMyOrders);
 
-// User cancel order
 router.put("/:id/cancel", protect, cancelOrder);
 
-// User requests return
 router.put("/:id/return", protect, requestReturn);
 
-// Generate invoice
 router.get("/:id/invoice", protect, generateInvoice);
 
-
-// ================= ADMIN ROUTES =================
-
-// MUST BE BEFORE /:id  or it will match wrong!
 router.get("/count", protectAdmin, async (req, res) => {
   try {
     const count = await Order.countDocuments();
@@ -48,19 +36,13 @@ router.get("/count", protectAdmin, async (req, res) => {
   }
 });
 
-// Get all orders
+
 router.get("/", protectAdmin, getOrders);
 
-// Get order by ID
 router.get("/:id", protectAdmin, getOrderById);
 
-
-// ================= FIXED ORDER FLOW =================
-
-// Admin approves return → MUST COME BEFORE "/:id"
 router.put("/:id/return/approve", protectAdmin, approveReturn);
 
-// Update order status (Processing, Shipped, Delivered)
 router.put("/:id", protectAdmin, updateOrderStatus);
 
 export default router;
